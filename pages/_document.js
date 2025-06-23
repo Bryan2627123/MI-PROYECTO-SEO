cd91d49fd11240b14d636865f0b76874dd83d3f9
Bryan Raul Pumacayo Banda <bryan.pumacayo@tecsup.edu.pe>
2025-06-22 23:19:11 -0500

    Agrega meta tag de verificación de Google Search Console

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <meta name="google-site-verification" content="7YI0gqZEJmLAXgTgddjwXc03x39e2VnHu7j9yMWsFR8" />
+      </Head>
       <body>
         <Main />
         <NextScript />
